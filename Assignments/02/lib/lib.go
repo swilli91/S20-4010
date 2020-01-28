f8afe0382742defa3db11c9c289e0347d2617869
Philip Schlump <pschlump@gmail.com>
2020-01-27 19:48:50 -0700

    Assinment 2 - partially complete

---
+++ Assignments/02/lib/lib.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// SVarI  marshals and indents a data structure into JSON.
+func SVarI(v interface{}) string {
+	// s, err := json.Marshal ( v )
+	s, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return fmt.Sprintf("Error:%s", err)
+	}
+	return string(s)
+}
+
+// Exists returns true if a file or directory exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
